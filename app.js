bc375bf4299a9e992ca2bf29a7775904c6b5a52f
WINdemon444 <windemon444@gmail.com>
2026-01-24 10:52:52 +0200

    link my_first_website

---
+++ app.js
@@ -0,0 +1,20 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+
+const PORT = process.env.PORT || 2015;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static(__dirname + "/views"));
+
+app.engine("ejs", require("ejs").renderFile);
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.listen(PORT);
+console.log(`Server is running on http://localhost:${PORT}`);
