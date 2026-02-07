bff2a1c2b6e834ad170a0d296648f19e6d1420a9
WINdemon444 <windemon444@gmail.com>
2026-02-07 18:51:52 +0200

    add video nd list of games

--- app.js
+++ app.js
@@ -15,6 +15,8 @@ app.set("view engine", "ejs");
 app.get("/", (req, res) => {
   res.render("index");
 });
-
+app.get("/send",(req, res) => {
+  res.render("send");
+});
 app.listen(PORT);
 console.log(`Server is running on http://localhost:${PORT}`);
